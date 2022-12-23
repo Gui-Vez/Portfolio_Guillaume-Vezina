9b81955f7b48762f508035e8638ef5100086d9e4
Gui-Vez <vezina.guillaume@outlook.com>
2022-12-23 03:20:10 -0500

    Ajout fonte Glacial Indifference

--- src/structure/composants/Navigation.jsx
+++ src/structure/composants/Navigation.jsx
@@ -1,10 +1,12 @@
 import '../../style/composants/Navigation.scss';
+// import Logo from '../../svg/Logo.svg';
+import {ReactComponent as Logo} from '../../svg/Logo.svg';
 
 export default function Entete(props)
 {
     return (
         <nav className='Navigation'>
-            
+            <Logo />
         </nav>
     )
 }
