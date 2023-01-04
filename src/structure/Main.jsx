42d1eeeba3bd8ca9ef7e531beb046054babd0faa
Gui-Vez <vezina.guillaume@outlook.com>
2023-01-04 03:00:30 -0500

    Page présentation contenu

--- src/structure/Main.jsx
+++ src/structure/Main.jsx
@@ -1,8 +1,8 @@
 import '../style/Main.scss';
 import Presentation from './composants/Presentation';
 import ListeProjets from './composants/ListeProjets';
-import Vagues from './composants/Vagues'
-import VaguesNoires from './composants/VaguesNoires'
+import Vagues       from './composants/Vagues';
+import VaguesNoires from './composants/VaguesNoires';
 
 export default function Main(props)
 {
--- src/structure/composants/Presentation.jsx
+++ src/structure/composants/Presentation.jsx
@@ -9,13 +9,11 @@ export default function Presentation(props)
                 <h2 className='titre-section'>Présentation</h2>
 
                 <div className='contenu-grille'>
-                    {/* <img className='image-profil' src={require("../../Images/Profil.jpg")} alt='Profil' /> */}
+                    <div className='image-profil' />
                     <div className='description'>
-                        <p>Je suis un jeune étudiant inspiré.</p>
-                        <br />
-                        <p>Passionné de design graphique et de programmation front-end, je suis quelqu'un qui adore collaborer en équipe.</p>
-                        <br />
-                        <p>Je suis fier d'avoir gagné en 2019 le premier prix de modélisation 3D de la région de Vaudreuil-Soulanges.</p>
+                        <p>Je suis un jeune étudiant <b>inspiré</b>.</p>
+                        <p>Passionné de design graphique et de programmation front-end, je suis quelqu'un qui adore <b>collaborer</b> en équipe.</p>
+                        <p>Je suis fier d'avoir gagné en 2019 le <b>premier prix</b> de modélisation 3D de la région de Vaudreuil-Soulanges.</p>
                     </div>
                 </div>
             </div>
