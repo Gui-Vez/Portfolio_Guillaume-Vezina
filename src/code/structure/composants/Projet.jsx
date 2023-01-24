754d628ad6ef158db26739ad58aef3998fd7a1da
Gui-Vez <vezina.guillaume@outlook.com>
2023-01-24 08:51:45 -0500

    Enlever fonctionnalité require() - Titre projet

--- src/code/structure/composants/Projet.jsx
+++ src/code/structure/composants/Projet.jsx
@@ -7,7 +7,7 @@ export default function Projet(props)
     return (
         <div className='Projet' id={"Projet-" + props.id}>
             <a href={props.url} target="_blank" rel="noopener noreferrer">
-                <img className='image-projet' alt="Projet" src={require("../../../images/Projets/Projet - " + props.titre + props.extension)} style={positionImage} />
+                <img className='image-projet' alt={"Projet - " + props.titre} src={"../../../images/Projets/Projet - " + props.titre + props.extension} style={positionImage} />
                 <div className='description-projet'>
                     <h4 className='description-projet-titre'>{props.titre}</h4>
                     <p className='description-projet-texte'>{props.description}</p>
