ffb6d6e1f0e39194e3628b7910d58ff23e5cb0c6
Gui-Vez <vezina.guillaume@outlook.com>
2023-01-23 19:21:57 -0500

    Dynamiser les projets
    En accédant à la base de donnée de Firestore

--- src/code/structure/Appli.jsx
+++ src/code/structure/Appli.jsx
@@ -2,36 +2,11 @@ import '../style/Appli.scss';
 import Entete from './Entete';
 import Main from './Main';
 import Footer from './Footer';
-import { useState, useEffect } from 'react';
-import { db } from '../data/firebase/config';
-import { collection, getDocs, query, orderBy } from 'firebase/firestore';
 
 function Appli()
 {
-    const [projets, setProjets] = useState([]);
-    const projetsRef = collection(db, "projets")
-    const projetsClasses = query(projetsRef, orderBy("id", "asc"))
-
-    useEffect(() =>
-    {
-        const getProjets = async () =>
-        {
-            const data = await getDocs(projetsClasses);
-            setProjets(data.docs.map((doc) => ({...doc.data(), id: doc.id })))
-        }
-        
-        getProjets();
-    }, [projetsClasses]);
-
     return (
         <div className="Appli">
-            
-            {/* Firebase test */}
-            {projets.map((projet =>
-            {
-                return <div> <h1>Titre: {projet.titre} </h1> </div>
-            }))}
-
             <Entete/>
             <Main/>
             <Footer/>
--- src/code/structure/composants/ListeProjets.jsx
+++ src/code/structure/composants/ListeProjets.jsx
@@ -1,25 +1,54 @@
 import '../../style/composants/ListeProjets.scss';
 import textes from '../../data/textes.json';
 import Projet from './Projet';
-import lesProjets from '../../data/projets.json';
+import projetsStatiques from '../../data/projets.json';
+import { useState, useEffect } from 'react';
+import { db } from '../../data/firebase/config.js';
+import { collection, getDocs, query, orderBy } from 'firebase/firestore';
 
 export default function ListeProjets(props)
 {
+    const [projetsDynamiques, setProjets] = useState(projetsStatiques);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() =>
+    {
+        const projetsRef = collection(db, "projets")
+        const projetsClasses = query(projetsRef, orderBy("id", "asc"))
+        const getProjets = async () =>
+        {
+            try
+            {
+                const data = await getDocs(projetsClasses);
+                setProjets(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
+            }
+            
+            catch (error)
+            {
+                setError(error);
+            }
+            
+            finally
+            {
+                setLoading(false);
+            }
+        }
+        
+        getProjets();
+    }, []);
+
+
     return (
         <section id='ListeProjets'>
             <h2 className='titre-section'>{textes.titre_section_2}</h2>
 
+            {loading && <div>Chargement...</div>}
+            {error && <div>Erreur: {error.message}</div>}
+
             <div className='Projets'>
                 {
-                    lesProjets.map(unProjet =>
-                                    <Projet
-                                        id={unProjet.id}
-                                        key={unProjet.id}
-                                        url={unProjet.url}
-                                        titre={unProjet.titre}
-                                        extension={unProjet.extension}
-                                        description={unProjet.description}
-                                        positionImage={unProjet.positionImage} />)
+                    projetsDynamiques.map(unProjet => <Projet {...unProjet} key={unProjet.id} />)
                 }
             </div>
         </section>
