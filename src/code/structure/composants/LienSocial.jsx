60c966d02f3942aabbabe58838afdf82b05d2004
Gui-Vez <vezina.guillaume@outlook.com>
2023-01-25 04:40:21 -0500

    Dynamiser l'ajout des Projets & Liens sociaux
    Afin de contrôler les cartes Projets et Liens sociaux sur Firestore.
    
    + Ajout images de défaut
    + Ajout messages d'avertissement en cas d'erreur

--- src/code/structure/composants/LienSocial.jsx
+++ src/code/structure/composants/LienSocial.jsx
@@ -1,17 +1,92 @@
 import '../../style/composants/LienSocial.scss';
+import defaultImage from '../../../images/Liens sociaux/default.png';
+import textes       from '../../data/textes.json';
+import validator from 'validator';
 
 export default function LienSocial(props)
 {
-    let grandeurImage = {transform: "scale(" + props.grandeur +")"};
+    let grandeurImage = {transform: "scale(" + (props.titre !== undefined ? props.grandeur : 1.5 ) + ")"}
+
+    let imageSrc = null;
+
+
+    async function checkUrl(url)
+    {
+        try
+        {
+            const response = await fetch(url);
+
+            if (response.ok)
+            {
+                return true;
+            }
+            
+            else
+            {
+                return false;
+            }
+        }
+        
+        catch (error)
+        {
+            return false;
+        }
+    }
+
+
+    function checkPattern(str)
+    {
+        const pattern = new RegExp(
+            '^([a-zA-Z]+:\\/\\/)?' + // protocol
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+            '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+            '(\\#[-a-z\\d_]*)?$', // fragment locator
+            'i'
+        );
+        return pattern.test(str);
+    }
+
+
+    try
+    {
+        if (props.imgUrl != undefined && validator.isURL(props.imgUrl) && checkUrl(props.imgUrl) && checkPattern(props.imgUrl))
+        {
+            imageSrc = props.imgUrl;
+        }
+        
+        else
+        {
+            try
+            {
+                imageSrc = require(`../../../images/Liens sociaux/${props.titre}${props.extension}`);
+            }
+            
+            catch (err)
+            {
+                imageSrc = defaultImage;
+            }
+        }
+    }
+
+    catch (err)
+    {
+        console.log(err);
+    }
+    
 
     return (
         <li className='LienSocial' id={"LienSocial-" + props.id}>
             <a href={props.url} target="_blank" rel="noopener noreferrer">
                 <div className='lien-social-image-contenant'>
-                    <img className='lien-social-image' src={require("../../../images/Liens sociaux/" + props.titre + props.extension)} alt={props.titre} style={grandeurImage} />
+                    <img className='lien-social-image'
+                         src={imageSrc}
+                         alt={props.titre || textes.liens_sociaux_titre_et_image_manquants}
+                         style={grandeurImage} />
                 </div>
                 <h3 className='lien-social-titre'>
-                    {props.titre}
+                    {props.titre || textes.liens_sociaux_titre_manquant}
                     <hr />
                 </h3>
             </a>
--- src/code/structure/composants/Projet.jsx
+++ src/code/structure/composants/Projet.jsx
@@ -1,20 +1,91 @@
 import '../../style/composants/Projet.scss';
-import defaultImage from '../../../images/Projets/default.jpg';
+import defaultImage from '../../../images/Projets/default.png';
+import textes       from '../../data/textes.json';
+import validator from 'validator';
 
 export default function Projet(props)
 {
     let positionImage = {objectPosition: props.positionImage};
 
+    let imageSrc = null;
+
+
+    async function checkUrl(url)
+    {
+        try
+        {
+            const response = await fetch(url);
+
+            if (response.ok)
+            {
+                return true;
+            }
+            
+            else
+            {
+                return false;
+            }
+        }
+        
+        catch (error)
+        {
+            return false;
+        }
+    }
+
+
+    function checkPattern(str)
+    {
+        const pattern = new RegExp(
+            '^([a-zA-Z]+:\\/\\/)?' + // protocol
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+            '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+            '(\\#[-a-z\\d_]*)?$', // fragment locator
+            'i'
+        );
+        return pattern.test(str);
+    }
+
+
+    try
+    {
+        if (props.imgUrl != undefined && validator.isURL(props.imgUrl) && checkUrl(props.imgUrl) && checkPattern(props.imgUrl))
+        {
+            imageSrc = props.imgUrl;
+        }
+        
+        else
+        {
+            try
+            {
+                imageSrc = require(`../../../images/Projets/${props.titre}${props.extension}`);
+            }
+            
+            catch (err)
+            {
+                imageSrc = defaultImage;
+            }
+        }
+    }
+    
+    catch (err)
+    {
+        console.log(err);
+    }
+      
+
     return (
         <div className='Projet' id={"Projet-" + props.id}>
             <a href={props.url} target="_blank" rel="noopener noreferrer">
             <img className='image-projet'
-                 alt={"Projet - " + props.titre}
-                 src={require("../../../images/Projets/" + props.titre + props.extension)} 
+                 alt={"Projet - " + (props.titre || textes.projet_titre_et_image_manquants)}
+                 src={imageSrc} 
                  style={positionImage} />
                 <div className='description-projet'>
-                    <h4 className='description-projet-titre'>{props.titre}</h4>
-                    <p className='description-projet-texte'>{props.description}</p>
+                    <h4 className='description-projet-titre'>{props.titre || textes.projet_titre_manquant}</h4>
+                    <p className='description-projet-texte'>{props.description || textes.projet_description_manquante}</p>
                 </div>
             </a>
         </div>
